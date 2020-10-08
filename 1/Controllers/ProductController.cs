c51aeb65ba4998e95bae7babb10b9998a4e6b1eb
sacc <50913285+filipkmiecik@users.noreply.github.com>
2020-10-08 21:15:29 +0200

    updated for displaying single product

--- 1/Controllers/ProductController.cs
+++ 1/Controllers/ProductController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _1.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _1.Controllers
 {
-    public class TestController : Controller
+    public class ProductController : Controller
     {
         public IActionResult Index()
         {
@@ -15,12 +16,19 @@ namespace _1.Controllers
 
         public String ReturnCurrentDate()
         {
-
+            DateTime thisDay = DateTime.Today;
+            return thisDay.ToString();
         }
 
         public IActionResult RedirectToGoogle()
         {
             return Redirect("http://www.google.com");
+        }
+
+        public IActionResult ProductModel()
+        {
+            var model = new ProductModel {name = "abc", description = "abc", price = 30 };
+            return View(model);
         }
     }
 }
--- 1/Models/ProductModel.cs
+++ 1/Models/ProductModel.cs
@@ -7,5 +7,8 @@ namespace _1.Models
 {
     public class ProductModel
     {
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
     }
 }
