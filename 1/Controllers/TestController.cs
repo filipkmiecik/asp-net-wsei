79dc17fdc7af59e11f97d0dcadbec72368c1bd04
sacc <50913285+filipkmiecik@users.noreply.github.com>
2020-10-08 20:29:09 +0200

    Initial commit

---
+++ 1/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _1.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public String ReturnCurrentDate()
+        {
+
+        }
+
+        public IActionResult RedirectToGoogle()
+        {
+            return Redirect("http://www.google.com");
+        }
+    }
+}
