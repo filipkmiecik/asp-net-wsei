f1db917aa3336672c7b06ae128058d910a47bc53
sacc <50913285+filipkmiecik@users.noreply.github.com>
2020-12-03 03:26:25 +0100

    Admin and Edit views, validation

--- StoreProject/StoreProject/Models/Product.cs
+++ StoreProject/StoreProject/Models/Product.cs
@@ -1,16 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace StoreProject.Models
 {
     public class Product
     {
         public int id { get; set; }
+        [Required(ErrorMessage = "Proszę wprowadzić nazwę produktu")]
         public string name { get; set; }
+        [Required(ErrorMessage = "Please wprowadzić opis produktu")]
         public string description { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue,
+            ErrorMessage = "Proszę wprowadzić dodatnią cenę")]
         public decimal price { get; set; }
+        [Required(ErrorMessage = "Please nadać kategorię")]
         public string category { get; set; }
 
     }
