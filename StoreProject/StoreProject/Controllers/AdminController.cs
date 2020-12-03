492b6af78a0ffcadf5418f57cec66dd54b91c7b0
sacc <50913285+filipkmiecik@users.noreply.github.com>
2020-12-03 02:59:25 +0100

    Lab 4 adding, deleting, AdminController, AdminLayout

---
+++ StoreProject/StoreProject/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreProject.Models;
+using System.Linq;
+
+namespace StoreProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ViewResult Index() => View(repository.Products);
+
+        public ViewResult Edit(int productId) => View(repository.Products.FirstOrDefault(p => p.id == productId));
+
+        [HttpPost]
+        public IActionResult Save(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = $"Zapisano {product.name}.";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Edit", product);
+            }
+        }
+        public ViewResult Create() => View("Edit", new Product());
+
+        [HttpPost]
+        public IActionResult Delete(int productId)
+        {
+            Product deletedProduct = repository.DeleteProduct(productId);
+            if (deletedProduct != null)
+            {
+                TempData["message"] = $"Usunięto {deletedProduct.name}.";
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- StoreProject/StoreProject/Models/EFProductRepository.cs
+++ StoreProject/StoreProject/Models/EFProductRepository.cs
@@ -13,5 +13,36 @@ namespace StoreProject.Models
             this.ctx = ctx;
         }
         public IQueryable<Product> Products => ctx.Products;
+
+        public void SaveProduct(Product product)
+        {
+            if (product.id == 0)
+            {
+                ctx.Products.Add(product);
+            }
+            else
+            {
+                Product dbEntry = ctx.Products.FirstOrDefault(p => p.id == product.id);
+                if(dbEntry != null)
+                {
+                    dbEntry.name = product.name;
+                    dbEntry.description = product.description;
+                    dbEntry.price = product.price;
+                    dbEntry.category = product.category;
+                }
+            }
+            ctx.SaveChanges();
+        }
+
+        public Product DeleteProduct(int productId)
+        {
+            Product dbEntry = ctx.Products.FirstOrDefault(p => p.id == productId);
+            if(dbEntry != null)
+            {
+                ctx.Products.Remove(dbEntry);
+                ctx.SaveChanges();
+            }
+            return dbEntry;
+        }
     }
 }
